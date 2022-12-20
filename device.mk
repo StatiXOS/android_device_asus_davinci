52c712f9902845451042bfe30929c53b46c4a2ab
Sourajit Karmakar <sourajitk@statixos.com>
2022-12-20 00:29:53 -0500

    davinci: Pick up QTI Vibrator AIDL split makefile
    Change-Id: Ia932ad3d307938fc624c790e75f41c9b55062f38

--- device.mk
+++ device.mk
@@ -391,6 +391,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+
 # WLAN
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
