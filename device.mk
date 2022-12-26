aa56e5b5191e83c72b7b78d2a9e97ac31f201078
Anay Wadhera <anay1018@gmail.com>
2022-12-26 00:27:20 -0500

    davinci: Build librmnetctl
    Change-Id: Ib907bc460726190d536b98c5e0b8efc4aae83f1b

--- device.mk
+++ device.mk
@@ -91,6 +91,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Data
+PRODUCT_PACKAGES += \
+    librmnetctl
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
