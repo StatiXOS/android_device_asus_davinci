75a271d0b90b1eb09855cd4fee7627367ccc7846
Sourajit Karmakar <sourajitk@statixos.com>
2022-12-26 00:28:01 -0500

    davinci: Build tinycompress and pal-1.0
    Change-Id: I601d1d501b0b479cda32cab1fbf26ea70aa38446

--- device.mk
+++ device.mk
@@ -44,7 +44,9 @@ $(call soong_config_set,android_hardware_audio,run_64bit,true)
 # Pick up split makefile
 $(call inherit-product, hardware/qcom-caf/sm8450/audio/configs/taro/taro.mk)
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service
+    android.hardware.audio.service \
+    libtinycompress \
+    vendor.qti.hardware.pal@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape_qssi/audio_policy_configuration.xml \
